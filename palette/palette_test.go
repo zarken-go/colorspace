3d2fad593d5dc8313e54eb786fe6b0094c8ee7b6
ottodashadow <ottodashadow@users.noreply.github.com>
2021-03-28 16:34:52 -0400

    100 percent coverage palette package

--- palette/palette_test.go
+++ palette/palette_test.go
@@ -9,12 +9,16 @@ import (
 func TestNewPalette(t *testing.T) {
 	m := make(map[uint32]int)
 	m[16711680] = 10 // #ff0000
+	m[16715535] = 1  // #ff0f0f
 	m[16715520] = 3  // #ff0f00
 	m[255] = 2       // #0000ff
 
 	p := New(m)
-	assert.Equal(t, []uint32{16711680, 255, 16715520}, p.Colors())
+	assert.Equal(t, []uint32{16711680, 255, 16715520, 16715535}, p.Colors())
 
-	merged := p.MergeColors(2)
+	merged := p.MergeColors(3)
 	assert.Equal(t, []uint32{16711680, 255}, merged.Colors())
+
+	mergedLimit := p.MergeColors(2)
+	assert.Equal(t, []uint32{16711680, 255}, mergedLimit.Colors())
 }
